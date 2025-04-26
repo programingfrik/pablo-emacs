17112e14fa4723ab1616a7fba571e5fe34da033e
Pablo Mercader Alcántara <programingfrik@gmail.com>
2025-04-26 18:49:07 -0400

    Agregando casos de prueba y la función de probar.

--- pruebas-pablo-emacs.el
+++ pruebas-pablo-emacs.el
@@ -2,3 +2,32 @@
 ;; Para hacer pruebas automatizadas de las funciones de pablo-emacs.el
 ;; Quiero simular algunos escenarios del fichero casos-prueba.txt
 
+(defun probar-todo ()
+  "Una función que ejecuta todas las pruebas."
+
+  ;; Recorrer todas las pruebas
+
+  ;; Las pruebas son funciones en este fichero "pruebas-pablo-emacs.el" cuyo nombre comienza con "prueba-"
+  ;; obarray
+  ;; find-lisp-object-file-name
+  
+  ;; Un conjunto de las pruebas vienen del fichero casos pruebas.
+
+  ;; Lee el fichero de las prueba
+  ;; (insert-file-contents file &optional visit beg end replace)
+
+  ;; Parte el fichero en pruebas individuales.
+
+  ;; Crea el escenario
+
+  ;; Ejecuta la prueba
+
+  ;; Si el resultado es el esperado la prueba pasó.
+
+  ;; Sino la prueba falló.
+
+  ;; Hacer la notificación y pasar a la siguiente prueba.
+  )
+
+
+
